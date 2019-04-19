21be5542badd69cb7bdc21d88e1c2c8e218263ac
TellesBruno <brunotellesdossantos@gmail.com>
2019-04-19 16:44:32 -0300

    Checkpoints implementados

---
+++ Assets/Scripts/CarRaceStats.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarRaceStats : MonoBehaviour
+{
+    public int voltas;
+    public bool checkP1;
+    public bool checkP2;
+    public bool checkP3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        voltas = 0;
+        checkP1 = false;
+        checkP2 = false;
+        checkP3 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void incrementoVoltas(){
+        if (checkP1 && checkP2 && checkP3){
+            voltas += 1;
+            Debug.Log(voltas);
+            checkP1 = false;
+            checkP2 = false;
+            checkP3 = false;
+        }
+    }
+
+    public void trueCheckP1 () {
+        if(checkP2 == false && checkP3 == false){
+            checkP1 = true;
+        } else {
+            Debug.Log("Wrong Way! Trapaceiro!");
+        }
+    }
+
+    public void trueCheckP2 () {
+        if (checkP1 && checkP3 == false){
+            checkP2 = true;
+        } else {
+            Debug.Log("Wrong Way! Trapaceiro!");
+        }
+    }
+
+    public void trueCheckP3 () {
+        if(checkP1 && checkP2){
+            checkP3 = true;
+        } else {
+            Debug.Log("Wrong Way! Trapaceiro!");
+        }
+    }
+
+}
--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    public int check;
-    public Finish Fin;
+    public CarRaceStats _car;
+    public int maxVoltas;
+    //public Finish Fin;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,33 @@ public class Checkpoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    Fin.PassCheck(check);      
+    //Fin.PassCheck(check);      
     }
+
+    void OnTriggerEnter(Collider other){
+        if (other.tag == "CarColl"){
+            _car = other.GetComponent<CarRaceStats>();
+            if (this.gameObject.name == "checkpoint_1"){
+                _car.trueCheckP1();
+            }
+            if (this.gameObject.name == "checkpoint_2"){
+                _car.trueCheckP2();
+            }
+            if (this.gameObject.name == "checkpoint_3"){
+                _car.trueCheckP3();
+            }
+
+            if (this.gameObject.name == "finishLine"){
+                _car.incrementoVoltas();
+                if (_car.voltas == maxVoltas){
+                    Debug.Log(other.gameObject.name);
+                    Debug.Log("Venceu a corrida");
+                    // #######################################################
+                    // ## Colocar aqui o codigo da condição de vitoria!!!!! ##
+                    // #######################################################
+                }
+            }
+        }
+    }
+
 }
