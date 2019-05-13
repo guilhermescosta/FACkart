15226be662f7f081811f39c3c3aff68a0371c850
gabrielbazei <gabriel.lima.bazei@hotmail.com>
2019-05-13 18:08:44 -0300

    Criação do MenuPrincipal
    Foi criado um menu principal com Skybox, Raw imagame e dois botões

---
+++ Assets/Scripts/MenuPrincial.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject mCamera;
+    
+    void Start()
+    {
+        
+    }
+    public void StartGame(){
+        SceneManager.LoadScene(1);
+    }
+    public void Quit() {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        mCamera.transform.Rotate(0,0.5f,0);
+    }
+}
